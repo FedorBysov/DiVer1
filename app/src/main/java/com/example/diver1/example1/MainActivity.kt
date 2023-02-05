46b6fafaf704d6ee13852a38ed0241ab167e710e
Fedor <bysov1987@mail.ru>
2023-02-05 14:42:15 +0300

    inject in class field, in constructor

--- app/src/main/java/com/example/diver1/example1/MainActivity.kt
+++ app/src/main/java/com/example/diver1/example1/MainActivity.kt
@@ -8,8 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//
-//        val activity = Activity()
-//        activity.keyboard.toString()
+
+        val activity = Activity()
+        activity.keyboard.toString()
     }
 }
